ff27740ca7132972b5bbe818bac2c109c40e561f
Darius K <darius.kaya@gmail.com>
2024-04-22 10:48:50 +0200

    1 - add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <h2>Copyright Darius</h2>
+        </footer>
+    )
+};
+
+export default Footer
